736fb74d74cd2b63f65ed3ca60ab07ead1829b9a
Fabricio de sousa <fabricio1234c@gmail.com>
2021-03-30 14:53:09 -0300

    adding more

---
+++ aula-01/app.js
@@ -0,0 +1,11 @@
+//const { nome, sobrebome, falaNome } = require('./aula-01');
+
+
+//console.log(falaNome());
+const path = require('path')
+const {Pessoa} = require('./aula-01')
+
+
+
+const p1 = new Pessoa('Luiz');
+console.log(p1);
---
+++ aula-01/aula-01.js
@@ -0,0 +1,22 @@
+
+{
+//const nome = 'Luiz'
+//const sobrenome = 'mirando'
+//
+//const falaNome = () => nome + ' ' + sobrenome;
+//
+//
+//exports.nome = nome;
+//exports.sobrenome = sobrenome;
+//exports.falaNome = falaNome;
+//this.qualquerCoisa = 'O que eu quiser exportar';
+
+
+}
+
+class Pessoa {
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+exports.Pessoa = Pessoa;
